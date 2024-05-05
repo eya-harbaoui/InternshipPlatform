1e4ba42a2f3bbbd0721060ec08576a90c2f80fc5
eya-harbaoui <102240094+eya-harbaoui@users.noreply.github.com>
2024-05-05 12:54:55 +0100

    editing manager domains

--- src/pages/ManagerDomains/ManagerDomains.js
+++ src/pages/ManagerDomains/ManagerDomains.js
@@ -1,4 +1,3 @@
-//import
 import React, { useState, useEffect } from "react";
 import "./ManagerDomains.css";
 import Navbar from "../../components/Navbar/Navbar";
@@ -7,11 +6,14 @@ import { MdOutlineContentPasteSearch } from "react-icons/md";
 import { ManagerNavbarLinks } from "../../components/Navbar/ManagerNavbarLinks";
 import DomainCard from "../../components/DomainCard/DomainCard";
 import { Modal, Input, Tag, Button } from "antd";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ManagerDomains = () => {
   const [data, setData] = useState([]);
   const [selectedDomain, setSelectedDomain] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [addModalVisible, setAddModalVisible] = useState(false);
   const [domainName, setDomainName] = useState("");
   const [competences, setCompetences] = useState([]);
   const [newCompetence, setNewCompetence] = useState("");
@@ -35,6 +37,7 @@ const ManagerDomains = () => {
   const handleDelete = async (id) => {
     try {
       await axios.delete(`http://localhost:8000/Domaines/${id}`);
+      toast.success("Domaine supprimé avec succès");
       fetchData();
     } catch (error) {
       console.error("Error deleting domain: ", error);
@@ -47,10 +50,28 @@ const ManagerDomains = () => {
         domainName,
         competences,
       });
+      toast.success("Domaine modifié avec succès");
       setModalVisible(false);
       fetchData();
     } catch (error) {
       console.error("Error updating domain: ", error);
+    }
+  };
+
+  const addDomain = async () => {
+    try {
+      await axios.post(`http://localhost:8000/Domaines`, {
+        domainName,
+        competences,
+      });
+      toast.success("Domaine ajouté avec succès");
+      setAddModalVisible(false);
+      setDomainName(""); // Vider le nom du domaine après l'ajout
+      setCompetences([]); // Vider les compétences après l'ajout
+      setNewCompetence(""); // Vider la nouvelle compétence après l'ajout
+      fetchData();
+    } catch (error) {
+      console.error("Error adding domain: ", error);
     }
   };
 
@@ -69,7 +90,7 @@ const ManagerDomains = () => {
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, []); // Correction: Ajout d'un tableau vide pour déclencher useEffect uniquement lors du montage initial
 
   return (
     <>
@@ -77,6 +98,14 @@ const ManagerDomains = () => {
         <Navbar links={ManagerNavbarLinks} />
         <h2 className="title-Manager-page">Gestion des domaines</h2>
         <MdOutlineContentPasteSearch className="icon-Manager-page" />
+        <div className="button-container">
+          <button
+            className="add-domain-button"
+            onClick={() => setAddModalVisible(true)}
+          >
+            Ajouter un domaine
+          </button>
+        </div>
         {data &&
           data.map((domaine, index) => (
             <DomainCard
@@ -85,7 +114,7 @@ const ManagerDomains = () => {
               handleEdit={() => {
                 handleEdit(domaine);
               }}
-              handleDeleteCompetence={() => {
+              handleDelete={() => {
                 handleDelete(domaine.id);
               }}
             />
@@ -100,6 +129,36 @@ const ManagerDomains = () => {
         <Input
           value={domainName}
           onChange={(e) => setDomainName(e.target.value)}
+        />
+        {competences.map((competence, index) => (
+          <Tag key={index} closable onClose={() => deleteCompetence(index)}>
+            {competence}
+          </Tag>
+        ))}
+        <Input
+          value={newCompetence}
+          onChange={(e) => setNewCompetence(e.target.value)}
+          placeholder="Nouvelle compétence"
+          onPressEnter={addCompetence}
+          suffix={<Button onClick={addCompetence}>Ajouter</Button>}
+        />
+      </Modal>
+      {/* New Modal for adding domain */}
+      <Modal
+        title="Ajouter un domaine"
+        visible={addModalVisible}
+        onOk={addDomain}
+        onCancel={() => {
+          setAddModalVisible(false);
+          setDomainName(""); // Vider le nom du domaine si annulé
+          setCompetences([]); // Vider les compétences si annulé
+          setNewCompetence(""); // Vider la nouvelle compétence si annulé
+        }}
+      >
+        <Input
+          value={domainName}
+          onChange={(e) => setDomainName(e.target.value)}
+          placeholder="Nom du domaine"
         />
         {competences.map((competence, index) => (
           <Tag key={index} closable onClose={() => deleteCompetence(index)}>
